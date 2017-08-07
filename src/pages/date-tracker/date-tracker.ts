39ccfc81445ffa3b2750d593d510977c28b5cbc0
Seth Tarrants <roguem99@live.com>
2017-08-06 21:37:47 -0400

    updated date-tracker to pull value entered and calculate time left

--- src/pages/date-tracker/date-tracker.ts
+++ src/pages/date-tracker/date-tracker.ts
@@ -1,32 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { TimerObservable } from 'rxjs/observable/TimerObservable';
 
 @Component({
   selector: 'page-date-tracker',
   templateUrl: 'date-tracker.html',
-  inputs: ['myDate']
-
+  inputs: ['myDate', 'resultDate'],
+  
 })
 
 export class DateTrackerPage {
-  myDate: any;
-  currentDate: any;
-  resultDate: number;
+  public myDate: String = new Date().toISOString();
+  public resultDate: number;
+  public currentDate: String;
+  // private tick: number;
 
 //ISO 8601 Datetime Format: YYYY-MM-DDTHH:mmZ
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-      
-      function calculateTimeLeft(selectedDate){
-        let date = selectedDate;
-        let currentDate = new Date();
-        console.log('selected Date ====>', selectedDate)
-        
-        return Math.floor((date - this.currentDate) / (1000*60*60*24));
-      }
+      this.currentDate = (new Date()).toISOString();
+      let resultDate = this.calculateTimeLeft();
+      // let timer = TimerObservable.create(2000, 1000).subscribe(t => { this.resultDate = t; });
+    }
+
+  calculateTimeLeft() {
+    let myDateInstance = new Date(this.myDate.substring(0, 10)).getTime();
+    let dateInstance = new Date().getTime();
+    console.log( myDateInstance);
+    console.log( dateInstance);
+    
+    // let resultDate = Math.floor((myDateInstance - dateInstance) / (1000*60*60*24));
+    let resultDate = Math.floor(myDateInstance - dateInstance);
+    console.log(resultDate);
+    // convert resultDate # of Days till release to a time format with #Months, #Days, #Hours, #Minutes, #Seconds
+    return resultDate;
+  }
 
 
-
+  getSecondsAsDigitalClock(resultDate: number) {
+        var sec_num = parseInt(resultDate.toString(), 10); // don't forget the second param
+        var days = Math.floor(sec_num / 30)
+        var hours   = Math.floor(sec_num / 3600);
+        var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+        var seconds = sec_num - (hours * 3600) - (minutes * 60);
+        var dayString = '';
+        var hoursString = '';
+        var minutesString = '';
+        var secondsString = '';
+        dayString = (hours < 10) ? "0" + days : days.toString();
+        hoursString = (hours < 10) ? "0" + hours : hours.toString();
+        minutesString = (minutes < 10) ? "0" + minutes : minutes.toString();
+        secondsString = (seconds < 10) ? "0" + seconds : seconds.toString();
+        return hoursString + ':' + minutesString + ':' + secondsString;
+  }
 
 
 
@@ -55,8 +81,5 @@ export class DateTrackerPage {
       //     return [days + 'd', hours + 'h', minutes + 'm', seconds + 's'].join(' ');
 
       //     }).call(this);
-
-  }
-
 
 }
